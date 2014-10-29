19bb56ce0a8f57657b537595258ea953efcb9336
William Benton <self@willbenton.com>
2014-10-29 10:49:06 -0500

    updated build defs

--- project/build.scala
+++ project/build.scala
@@ -2,16 +2,24 @@ import sbt._
 import sbt.Keys._
 
 object CopricapoBuild extends Build {
-
+  val VERSION = "0.1-SNAPSHOT"
+  val SCALA_VERSION = "2.10.4"
+  
   lazy val copricapo = Project(
     id = "copricapo",
     base = file("."),
     settings = Project.defaultSettings ++ Seq(
       name := "copricapo",
       organization := "com.freevariable",
-      version := "0.1-SNAPSHOT",
-      scalaVersion := "2.10.4"
-      // add other settings here
+      version := VERSION,
+      scalaVersion := SCALA_VERSION,
+      libraryDependencies ++= Seq(
+        "com.typesafe.slick" %% "slick" % "2.0.2",
+        "org.postgresql" % "postgresql" % "9.3-1102-jdbc41",
+        "org.slf4j" % "slf4j-nop" % "1.6.4",
+        "org.scalatest" %% "scalatest" % "2.0" % "test",
+        "org.scala-lang" % "scala-reflect" % SCALA_VERSION
+      )
     )
   )
 }
