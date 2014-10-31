acae13851bd11d9623e207636d027e7995e39805
William Benton <self@willbenton.com>
2014-10-31 16:13:33 -0500

    further simplifications

--- src/main/scala/com/freevariable/copricapo/Importer.scala
+++ src/main/scala/com/freevariable/copricapo/Importer.scala
@@ -4,8 +4,9 @@ import scala.slick.direct._
 import scala.slick.direct.AnnotationMapper._
 import scala.slick.driver.PostgresDriver
 import scala.slick.driver.PostgresDriver.simple._
-import scala.reflect.runtime.universe
 import scala.slick.jdbc.StaticQuery.interpolation
+
+import scala.reflect.runtime.universe
 
 import com.freevariable.copricapo.schema._
 
@@ -14,7 +15,7 @@ object Importer {
   import org.json4s.ext._
   import org.json4s.JsonDSL._
   import org.joda.time.DateTime
-  import org.json4s.jackson.JsonMethods.{parse, render, compact}
+  import org.json4s.jackson.JsonMethods.{parse => parseJson, render => renderJson, compact}
   
   object JSONImplicits {
     private def tupleIfPresent[T](name: String, v: Option[T])
@@ -27,15 +28,12 @@ object Importer {
       ("timestamp" -> (new DateTime(m.timestamp.getTime()).toString)) ~
       tupleIfPresent("topic", m.topic) ~
       tupleIfPresent("category", m.category) ~
-      ("msg" -> parseJSON(m._Msg)) ~
+      ("msg" -> parseJson(m._Msg)) ~
       tupleIfPresent("msg_id", m.msgId) ~
       tupleIfPresent("source_name", m.sourceName) ~
       tupleIfPresent("source_version", m.sourceVersion)
     }
   }
-  
-  def parseJSON(s: String) = parse(s)
-  def renderJSON[T](v: T)(implicit c: T => JValue) = compact(render(v))
   
   def apply(dburl: String, output_dir: String, limit: Int = 0) {
     import JSONImplicits._
@@ -45,12 +43,12 @@ object Importer {
     Database.forURL(dburl, driver="org.postgresql.Driver") withSession { implicit session =>
       val messages = TableQuery[Tables.Messages]
       
-      (if (limit == 0) messages else messages.take(limit)).elements foreach { m =>
+      (if (limit == 0) messages else messages.take(limit)) foreach { m =>
         val id = m.id
         val outputFile = s"$output_dir/$id.json"
         println(s"rendering to $outputFile")
         val pw = new java.io.PrintWriter(new java.io.File(outputFile))
-        pw.println(renderJSON(m))
+        pw.println(compact(renderJson(m)))
         pw.close
       }
     }
